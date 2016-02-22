64d90db2092d725763e9c87c1a4bac07ea1ec9c5
Alex Garland <jeffrey.garland@yale.edu>
2016-02-22 13:25:36 -0500

    Worked on question 3

--- PS4.R
+++ PS4.R
@@ -96,3 +96,80 @@ obs <- 1073
 N <- 25
 F_stat3 <- obs * (obs - N - 1) * test_stat3 / (N * (obs - 2))
 p_val3 <- pf(F_stat3, N, obs-N-1, 0, lower.tail = FALSE)
+
+#Question 3g
+one_v = rep(1, 25)
+tan_weights <- (v_inv3 %*% (mean_beme - mean_rf)) / (as.vector(t(one_v) %*% v_inv3 %*% (mean_beme - mean_rf)))
+tan_excess_return <- excess_returns_arr3 %*% tan_weights
+
+model_list4 <- lm(excess_returns_arr3 ~ tan_excess_return)
+intercepts4 <- model_list4$coefficients[1,]
+tan_sharpe <- mean(tan_excess_return) / sd(tan_excess_return)
+
+test_stat4 <- (t(intercepts4) %*% v_inv3 %*% intercepts4) / (1+tan_sharpe^2)
+obs <- 1073
+N <- 25
+F_stat4 <- obs * (obs - N - 1) * test_stat4 / (N * (obs - 2))
+p_val4 <- pf(F_stat4, N, obs-N-1, 0, lower.tail = FALSE)
+
+
+#Question 3h
+even_years <- subset(beme_port, as.integer(beme_port$Date) %% 2 == 0) 
+odd_years <- subset(beme_port, as.integer(beme_port$Date) %% 2 == 1)
+
+even_training <- subset(even_years, as.integer(even_years$Date * 100) %% 2 == 1)
+even_test <- subset(even_years, as.integer(even_years$Date * 100) %% 2 == 0)
+odd_training <- subset(odd_years, as.integer(odd_years$Date * 100) %% 2 == 0)
+odd_test <- subset(odd_years, as.integer(odd_years$Date * 100) %% 2 == 1)
+
+training_df <- rbind(even_training, odd_training)
+test_df <- rbind(even_test, odd_test)
+
+vcov_mat4 = cov(training_df[2:26])
+v_inv4 = ginv(vcov_mat4)
+training_ret = as.matrix(training_df[,2:26])
+avg_train_rtn <- apply(training_df[,2:26], 2, mean, na.rm=TRUE)
+avg_train_rtn2 <- avg_train_rtn - mean_rf
+tan_weights2 <- (v_inv4 %*% avg_train_rtn2) / (as.vector(t(one_v) %*% v_inv4 %*% avg_train_rtn2))
+
+vcov_mat5 = cov(test_df[2:26])
+v_inv5 = ginv(vcov_mat5)
+test_ret <- as.matrix(test_df[,2:26])
+avg_test_rtn <- apply(test_df[,2:26], 2, mean)
+avg_test_rtn2 <- avg_test_rtn - mean_rf
+tan_weights3 <- (v_inv5 %*% avg_test_rtn2) / (as.vector(t(one_v) %*% v_inv5 %*% avg_test_rtn2))
+
+first_half_of_returns <- test_ret %*% tan_weights2
+second_half <- training_ret %*% tan_weights3
+
+total_ret <- as.numeric()
+for (i in 1:1073){
+  if (as.integer(beme_port$Date) %% 2 == 0 && as.integer(beme_port$Date * 100) %% 2 == 1){
+    holder <- excess_returns_arr3[i,] %*% tan_weights3
+    total_ret <- c(total_ret, holder)
+  }
+  else if (as.integer(beme_port$Date) %% 2 == 1 && as.integer((beme_port$Date * 100) %% 2 == 0)){
+    holder <- excess_returns_arr3[i,] %*% tan_weights3
+    total_ret <- c(total_ret, holder)
+  }
+  else{
+    holder <- excess_returns_arr3[i,] %*% tan_weights2
+    total_ret <- c(total_ret, holder)
+  }
+}
+
+tan_sharpe2 <- mean(total_ret) / sd(total_ret)
+
+model_list5 <- lm(excess_returns_arr3 ~ total_ret)
+intercepts5 <- model_list5$coefficients[1,]
+
+test_stat5 <- (t(intercepts5) %*% v_inv3 %*% intercepts5) / (1+tan_sharpe2^2)
+obs <- 1073
+N <- 25
+F_stat5 <- obs * (obs - N - 1) * test_stat5 / (N * (obs - 2))
+p_val5 <- pf(F_stat5, N, obs-N-1, 0, lower.tail = FALSE)
+
+#Question 3i
+one_v <- rep(1, 30)
+industry_tan <- (v_inv %*% (mean_industry - mean_rf)) / (as.vector(t(one_v) %*% v_inv %*% (mean_industry - mean_rf)))
+ind_tan_returns <- excess_returns_arr %*% industry_tan
