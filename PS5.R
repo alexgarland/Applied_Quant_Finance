6602e99fdfdfb621cd3f7a55eb98b2a82649a9d0
Alex Garland <jeffrey.garland@yale.edu>
2016-02-28 01:53:14 -0500

    Finished a and b

--- PS5.R
+++ PS5.R
@@ -28,6 +28,8 @@ momentum_portfolio_momentum[momentum_portfolio_momentum <= -.99] <- NA
 
 FF_Factors <- read_csv("PS5_ff_factors.csv")
 FF_Factors <- FF_Factors / 100
+FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)` <- 100*FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`
+FF_Factors[FF_Factors <= -.99] <- NA
 
 #Expand the BEME out from an annualized to a monthly stat
 expanded_beme <- beme_portfolio_beme[1,]
@@ -36,3 +38,28 @@ for (i in 1:90){
     expanded_beme <- rbind(expanded_beme, beme_portfolio_beme[i,])
   }
 }
+
+#Question A
+reg_mkt <- lm(FF_Factors$`Mkt-RF`~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_smb <- lm(FF_Factors$SMB~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_hml <- lm(FF_Factors$HML ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_rf <- lm(FF_Factors$RF ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_umd <- lm(FF_Factors$UMD[7:1073] ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`[7:1073])
+
+#Question B
+mean_rf <- mean(FF_Factors$`Mkt-RF`)
+
+beme_portfolio <- beme_portfolio - mean_rf
+momentum_portfolio <- momentum_portfolio - mean_rf
+beme_portfolio$Date <- beme_portfolio$Date + mean_rf
+momentum_portfolio$`Size and Ret212` <- momentum_portfolio$`Size and Ret212` + mean_rf
+
+reg_small_value <- lm(beme_portfolio$`Small Size High BE/ME` ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_small_growth <- lm(beme_portfolio$`Small Size Low BE/ME` ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_big_growth <- lm(beme_portfolio$`Big Size Low BE/ME` ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+reg_big_value  <- lm(beme_portfolio$`Big Size High BE/ME` ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`)
+
+reg_small_losers <- lm(momentum_portfolio$`Small Size  & Low Ret212`[7:1069] ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`[7:1069])
+reg_small_winners <- lm(momentum_portfolio$`Small Size  & High Ret212`[7:1069] ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`[7:1069])
+reg_big_losers <- lm(momentum_portfolio$`Big Size  & Low Ret212`[7:1069] ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`[7:1069])
+reg_big_winners <- lm(momentum_portfolio$`Big Size  & High Ret212`[7:1069] ~ FF_Factors$`Recession indicator (1=recession, 0=non-recesstion)`[7:1069])
