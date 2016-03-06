af61721952cda0890bf2430882c1f624ab627f5b
Alex Garland <jeffrey.garland@yale.edu>
2016-03-06 15:14:47 -0500

    Did part a

--- PS6.R
+++ PS6.R
@@ -6,3 +6,33 @@ source('~/Applied_Quant_Finance/PS6_helper.R')
 industry <- read_csv("PS6_Industry.csv")
 
 industry <- as.matrix(industry/100)
+
+#Question A
+best_performers <- t(apply(industry[,2:31], 1, find_three_highest))
+worst_performers <- t(apply(industry[,2:31], 1, find_three_lowest))
+
+mean_best <- c()
+mean_worst <- c()
+for (i in 2:1069){
+  mean_best <- c(mean_best, mean(industry[i, best_performers[(i-1),]]))
+  mean_worst <- c(mean_worst, mean(industry[i, worst_performers[(i-1),]]))
+}
+
+m_best <- mean(mean_best)
+m_worst <- mean(mean_worst)
+
+std_best <- sd(mean_best) / sqrt(1068)
+std_worst <- sd(mean_worst) / sqrt(1068)
+
+t_best <- m_best/ std_best
+t_worst <- m_worst/ std_worst
+
+sr_best <- t_best / sqrt(1068)
+sr_worst <- t_worst / sqrt(1068)
+
+ff_factors <- read_csv("PS6_FF.csv") / 100
+market_auto = cov(ff_factors$`Mkt-RF`[2:1073], ff_factors$`Mkt-RF`[1:1072])
+cross_variance <- var(apply(industry[,2:31], 2, mean))
+market_model <- lm(industry[,2:31] ~ ff_factors$`Mkt-RF`[1:1069])
+betas <- coef(market_model)[2,]
+var_beta <- var(betas)
--- PS6_helper.R
+++ PS6_helper.R
@@ -1,3 +1,9 @@
 find_three_highest <- function(entered_row){
-  return(sort(entered_row, decreasing=T)[1:3])
+  n <- order(entered_row, decreasing=T)[1:3]
+  return(n+1)
+}
+
+find_three_lowest <- function(entered_row){
+  n <- order(entered_row, decreasing=F)[1:3]
+  return(n+1)
 }
