098b3bb0a74eeebba518109feb89cb17abd5cd25
Alex Garland <jeffrey.garland@yale.edu>
2016-03-29 14:51:01 -0400

    JSON filtering in progress

--- Project/extract_run_python.sh
+++ Project/extract_run_python.sh
@@ -1,3 +1,9 @@
 directory="~/Applied_Quant_Finance/27"
 cd ~/Applied_Quant_Finance/27
-find . -name "*.bz2" | while read filename; do bzip2 -d "`dirname "$filename"`" "$filename"; done;
+
+#Create the relevant json files and run the relevant python file
+find . -name "*.bz2" | while read filename; do
+  json_name="${filename%.*}"
+  bzip2 -d "`dirname "$filename"`" "$filename";
+  python relevant_tweets.py $json_name
+done;
--- Project/relevant_tweets.py
+++ Project/relevant_tweets.py
@@ -1,1 +1,29 @@
-import os, sys
+import os, sys, json
+
+directory = "/home/alex/Applied_Quant_Finance/27/"
+os.chdir(directory)
+
+filename = sys.argv[-1]
+
+tweets = []
+exceptions = 0
+tries = 0
+
+with open(filename) as f:
+    for line in f:
+        try:
+            data = json.loads(line)
+            tweet = data['text']
+            if ":-)" not in tweet:
+                continue
+            else:
+                tweets.append(tweet)
+            tries +=1
+        except:
+            exceptions += 1
+            continue
+
+print len(tweets)
+print tweets
+print "Tries:", tries
+print "Exceptions:", exceptions
