edcc0f5ce930b49c508849e7e86bfb5a16a4fb89
Alex Garland <jeffrey.garland@yale.edu>
2016-04-04 15:44:26 -0400

    Started PS7, made a few modifications to the tweet searching software.

---
+++ Problem Sets/PS7.R
@@ -0,0 +1,59 @@
+library(readr)
+library(MASS)
+
+setwd("~/Applied_Quant_Finance/Problem Sets")
+rm(list=ls())
+
+one_month <- read_csv("PS7_1Month.csv")
+one_year <- read_csv("PS7_212Month.csv")
+five_years <- read_csv("PS7_1360Month.csv")
+
+one_month <- one_month / 100
+one_year <- one_year / 100
+five_years <- five_years / 100
+
+one_month$Jan <- 0
+one_year$Jan <- 0
+five_years$Jan <- 0
+
+one_month$Dec <- 0
+one_year$Dec <- 0
+five_years$Dec <- 0
+
+month_indicator <- one_month$Date %% 1
+month_list <- (month_indicator - .01 < .001)
+month_list <- which(month_list %in% TRUE)
+dec_list <- ((month_indicator - .12 < .001) & (month_indicator - .12 > -.001))
+dec_list <- which(dec_list %in% TRUE)
+
+one_month$Jan[month_list] <- 1
+one_year$Jan[month_list] <- 1
+five_years$Jan[month_list] <- 1
+
+one_month$Dec[dec_list] <- 1
+one_year$Dec[dec_list] <- 1
+five_years$Dec[dec_list] <- 1
+
+one_month[one_month <= -.99] <- NA
+one_year[one_year <= -.99] <- NA
+five_years[five_years <= -.99] <- NA
+
+ff_factors <- read_csv("PS6_FF.csv") / 100
+ff_factors[ff_factors <= -.99] <- NA
+mean_rf <- mean(ff_factors$RF)
+
+one_year <- one_year[12:1074,] 
+five_years <- five_years[60:1074,]
+
+one_month <- as.matrix(one_month)
+one_year <- as.matrix(one_year)
+five_years <- as.matrix(five_years)
+
+month_jan <- lm(one_month[,2:26] ~ one_month[,27])
+year_jan <- lm(one_year[,2:26] ~ one_year[,27])
+five_years_jan <- lm(five_years[,2:26] ~ five_years[,27])
+
+month_both <- lm(one_month[,2:26] ~ one_month[,27] + one_month[,28])
+year_both <- lm(one_year[,2:26] ~ one_year[,27] + one_year[,28])
+five_years_both <- lm(five_years[,2:26] ~ five_years[,27] + five_years[,28])
+
--- Project/relevant_tweets.py
+++ Project/relevant_tweets.py
@@ -8,18 +8,19 @@ filename = sys.argv[-1]
 tweets = []
 exceptions = 0
 tries = 0
+search = ["James Franco", "Seth Rogen", "Jonah Hill"]
 
 with open(filename) as f:
     for line in f:
         try:
             data = json.loads(line)
             tweet = data['text']
-            if ":-)" not in tweet:
-                tries += 1
-                continue
-            else:
+            if any(term in tweet for term in search):
                 tweets.append(tweet)
                 tries +=1
+            else:
+                tries += 1
+                continue
         except:
             exceptions += 1
             continue
@@ -28,3 +29,5 @@ os.remove(filename)
 
 with open (filename, 'wb') as f:
     json.dump(tweets, f)
+
+print(len(tweets))
