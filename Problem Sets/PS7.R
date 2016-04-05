7ee5774b71a2b10f9892db4a9656d76144db5221
Alex Garland <jeffrey.garland@yale.edu>
2016-04-04 20:06:21 -0400

    Working on D

--- Problem Sets/PS7.R
+++ Problem Sets/PS7.R
@@ -34,13 +34,21 @@ one_month$Dec[dec_list] <- 1
 one_year$Dec[dec_list] <- 1
 five_years$Dec[dec_list] <- 1
 
-one_month[one_month <= -.99] <- NA
-one_year[one_year <= -.99] <- NA
-five_years[five_years <= -.99] <- NA
+one_month[one_month < -.99] <- NA
+one_year[one_year < -.99] <- NA
+five_years[five_years < -.99] <- NA
 
 ff_factors <- read_csv("PS6_FF.csv") / 100
 ff_factors[ff_factors <= -.99] <- NA
 mean_rf <- mean(ff_factors$RF)
+
+one_month <- one_month - mean_rf
+one_year <- one_year - mean_rf
+five_years <- five_years - mean_rf
+
+one_month$Date <- one_month$Date + mean_rf
+one_year$Date <- one_year$Date + mean_rf
+five_years$Date <- five_years$Date + mean_rf
 
 one_year <- one_year[12:1074,] 
 five_years <- five_years[60:1074,]
@@ -57,3 +65,35 @@ month_both <- lm(one_month[,2:26] ~ one_month[,27] + one_month[,28])
 year_both <- lm(one_year[,2:26] ~ one_year[,27] + one_year[,28])
 five_years_both <- lm(five_years[,2:26] ~ five_years[,27] + five_years[,28])
 
+all_info <- merge(one_month, one_year, by = "Date")
+all_info <- merge(all_info, five_years, by = "Date")
+
+port_coef <- list()
+
+for (i in 2:1015){
+  for (j in 2:26){
+    model <- tryCatch({
+      lm(all_info[i,j] ~ all_info[(i-1),j] + 
+                           all_info[(i-1),(j+27)] + all_info[(i-1),(j+54)])}, 
+      error = function(e){ return(NA) })
+    if (!is.na(model)){
+      
+    }
+  }
+  for (j in 29:53){
+    model <- tryCatch({
+      lm(all_info[i,j] ~ all_info[(i-1),(j-27)] + 
+           all_info[(i-1),(j)] + all_info[(i-1),(j+27)])},
+      error = function(e){  return(NA)  })
+  }
+  for (j in 56:80){
+    model <- tryCatch({
+      lm(all_info[i,j] ~ all_info[(i-1),(j-54)] + 
+           all_info[(i-1),(j-27)] + all_info[(i-1),(j)])},
+      error = function(e){  return(NA)  })
+  }
+}
+
+for (i in 2:1015){
+  
+}
