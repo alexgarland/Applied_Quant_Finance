bee50ef9dc41a5181d2dd641f4f652582ba06a16
Alex Garland <jeffrey.garland@yale.edu>
2016-05-03 12:48:58 -0400

    Updated search script with terms

--- Project/relevant_tweets.py
+++ Project/relevant_tweets.py
@@ -8,7 +8,38 @@ filename = sys.argv[-1]
 tweets = []
 exceptions = 0
 tries = 0
-search = ["James Franco", "Seth Rogen", "Jonah Hill"]
+search = ["3M", "MMM",
+            "American Express", "AXP", "AMEX",
+            "Apple", "AAPL", "iPhone",
+            "AT&T", "ATT",
+            "Boeing",
+            "Caterpillar",
+            "Chevron", "CVX",
+            "Cisco", "CSCO",
+            "Coca-Cola", "KO", "Coke",
+            "DuPont", "DD",
+            "Exxon", "Mobil", "XOM",
+            "General Electric",
+            "Google", "GOOG",
+            "Goldman", "GS",
+            "Home Depot", "HD",
+            "IBM",
+            "Intel",
+            "Johnson & Johnson", "Johnson and Johnson", "JNJ",
+            "JPMorgan", "JPM", "JP Morgan",
+            "McDonald", "Golden Arches",
+            "Merck", "MRK",
+            "Microsoft", "MSFT", "Winows",
+            "Nike", "NKE",
+            "Pfizer", "PFE",
+            "Proctor & Gamble", "P&G", "P & G", "PG",
+            "Travelers", "TRV",
+            "UnitedHealth", "United Health", "UNH",
+            "UTX", "United Technologies",
+            "Verizon",
+            "Visa",
+            "Wal-Mart", "Walmart", "WMT",
+            "Disney"]
 
 with open(filename) as f:
     for line in f:
